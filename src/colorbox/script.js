6b2d0a96c6e5ebb8478887b9bfe74ce9e12ce628
Human0467 <mustchjo@gmail.com>
2024-10-24 14:18:57 +0100

    some refactoring, use console.dir, use references, remove TODOs (saved files before adding this time)

--- src/colorbox/script.js
+++ src/colorbox/script.js
@@ -1,5 +1,5 @@
-// TODO: add an event listener to the .box element that toggles the .red class
-const box = document.querySelector('.box')
+// add an event listener to the .box element that toggles the .red class
+const box = document.querySelector('.box');
 box.addEventListener('click', () => {
-    box.classList.toggle('red')
+    box.classList.toggle('red');
 })
--- src/counter/script.js
+++ src/counter/script.js
@@ -1,14 +1,14 @@
 // increment and decrement the count when the buttons are clicked
 
-const num = document.querySelector('#count')
-var current = num.innerText
+const num = document.querySelector('#count');
+var current = num.innerText;
 
 const decButton = document.querySelector('#decrement')
 decButton.addEventListener('click', () => {
-    num.innerText = current--
+    num.innerText = current--;
 })
 
 const incButton = document.querySelector('#increment')
 incButton.addEventListener('click', () => {
-    num.innerText = current++
+    num.innerText = current++;
 })
