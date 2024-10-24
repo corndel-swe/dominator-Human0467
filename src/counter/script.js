bfe85ec3cefd8ca23e5d96d06992dea73bd1b4c7
Human0467 <mustchjo@gmail.com>
2024-10-24 11:08:50 +0100

    progress on counter

--- src/counter/script.js
+++ src/counter/script.js
@@ -1,1 +1,14 @@
 // TODO: increment and decrement the count when the buttons are clicked
+
+const num = document.querySelector('#count')
+var current = num.innerText
+
+const decButton = document.querySelector('#decrement')
+decButton.addEventListener('click', () => {
+    num.innerText = current--
+})
+
+const incButton = document.querySelector('#increment')
+incButton.addEventListener('click', () => {
+    num.innerText = current++
+})
